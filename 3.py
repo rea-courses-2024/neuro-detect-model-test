135f48ea7c389f509d651ac9990d4d34312f8abf
REU learning <kaitomylayz@gmail.com>
2024-11-14 18:24:12 +0300

    new version 14.11.2024

--- 3.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Initialize the YOLO model
-model = YOLO("yolo11n.pt")
-
-# Tune hyperparameters on COCO8 for 30 epochs
-model.tune(data="YOLO_dataset/data.yaml", epochs=5, iterations=5, optimizer="AdamW", plots=False, save=False, val=False)
